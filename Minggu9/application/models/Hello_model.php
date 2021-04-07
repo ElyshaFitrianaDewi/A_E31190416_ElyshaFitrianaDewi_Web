ca60645a2cc1123318a44af36a18d0fc5a9bce69
ElyshaFitrianaDewi <80306258+ElyshaFitrianaDewi@users.noreply.github.com>
2021-04-07 10:02:18 +0700

    Update Hello_model.php

--- Minggu9/application/models/Hello_model.php
+++ Minggu9/application/models/Hello_model.php
@@ -5,8 +5,8 @@ class Hello_model extends CI_Model{
     public $alert = 'Hello Wolrd dari CI Model';
     
     public $mahasiswa = [
-			["nama"=>"Adya Zalfa","nim"=>"E31190957","gol"=>"B"],
-			["nama"=>"Vivalvia ","nim"=>"E31190785","gol"=>"A"],
-			["nama"=>"Shindi Fatikah","nim"=>"E31191120","gol"=>"B"] 
+			["nama"=>"Elysha","nim"=>"E31190416","gol"=>"A"],
+			["nama"=>"Adya ","nim"=>"E31190957","gol"=>"B"],
+			["nama"=>"Tiyas","nim"=>"E31190397","gol"=>"A"] 
 		];
 }
