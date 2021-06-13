e91d0454481f4d35e23dc88423c09dedb9b1dac2
Elysha2812 <elyshafitrianadewi@gmail.com>
2021-06-13 18:32:42 +0700

    E31190416_ElyshaFitrianaDewi_Gol.A_Android

---
+++ API_Volley/app/src/main/java/com/elyshafitriana/api_volley/Util/ServerAPI.java
@@ -0,0 +1,8 @@
+package com.elyshafitriana.api_volley.Util;
+
+public class ServerAPI {
+    public static final String URL_DATA = "http://192.168.162.221/TugasWebFramework/A_E31190416_ElyshaFitrianaDewi_Web/Rest_API/Mahasiswa/Api";
+    public static final String URL_INSERT = "http://192.168.162.221/TugasWebFramework/A_E31190416_ElyshaFitrianaDewi_Web/Rest_API/Mahasiswa/ApiInsert";
+    public static final String URL_DELETE = "http://192.168.162.221/TugasWebFramework/A_E31190416_ElyshaFitrianaDewi_Web/Rest_API/Mahasiswa/ApiDelete";
+    public static final String URL_UPDATE = "http://192.168.162.221/TugasWebFramework/A_E31190416_ElyshaFitrianaDewi_Web/Rest_API/Mahasiswa/ApiUpdate";
+}
